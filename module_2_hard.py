d3b2f1783387251dc2ddb38d8949e5ec2f7ef749
IrinaDem <starshinova9297@gmail.com>
2024-10-14 17:36:30 +0300

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,11 +1,12 @@
 def find_password(n):
     result = ""
-    for i in range(1, n // 2 + 1):
-        sum_nbr = i + (n - i)
-        if n % sum_nbr == 0:
-            result += str(i) + str(n - i)
+    for d in range(1, n + 1):
+        if n % d == 0: 
+            for i in range(1, d // 2 + 1):
+                j = d - i
+                if i + j == d:
+                    result += str(i) + str(j)
     return result
-
 
 n = int(input("Введите число от 3 до 20: "))
 password = find_password(n)
