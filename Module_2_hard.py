188ee111f86f1b74c22ba084a07db0ebfadf0530
IrinaDem <starshinova9297@gmail.com>
2024-10-14 19:53:49 +0300

    Add files via upload

--- Module_2_hard.py
+++ Module_2_hard.py
@@ -1,13 +1,17 @@
 def find_password(n):
     result = ""
     for d in range(1, n + 1):
-        if n % d == 0:
-            for i in range(1, d // 2 + 1):
+        if n % d == 0: 
+            for i in range(1, d):
                 j = d - i
-                if i + j == d:
+                if i < j and n % (i + j) == 0:
                     result += str(i) + str(j)
+
     return result
 
 n = int(input("Введите число от 3 до 20: "))
-password = find_password(n)
-print("Пароль:", password)
+if 3 <= n <= 20:
+    password = find_password(n)
+    print("Пароль:", password)
+else:
+    print("Число должно быть от 3 до 20.")
